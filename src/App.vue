dcd807a494f9af412af592cc84071b0006560ffc
longshihui <391424924@qq.com>
2022-04-15 10:23:05 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>App</div>
+</template>
+
+<script lang="ts">
+    export default {
+        name: 'App'
+    }
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ src/main.js
@@ -0,0 +1,10 @@
+"use strict";
+exports.__esModule = true;
+var vue_1 = require("vue");
+var App_vue_1 = require("./App.vue");
+new vue_1["default"]({
+    el: '#app',
+    render: function (h) {
+        return h(App_vue_1["default"]);
+    }
+});
